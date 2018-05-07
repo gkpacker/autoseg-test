32afcfb9e776ff437fbc17389e7add618c2f6701
Gabriel Packer <ggkpacker@gmail.com>
2018-05-07 15:26:33 -0300

    add new and create actions

--- app/controllers/task_lists_controller.rb
+++ app/controllers/task_lists_controller.rb
@@ -1,4 +1,18 @@
 class TaskListsController < ApplicationController
   def index
   end
+
+  def new
+    @task_list = TaskList.new
+  end
+
+  def create
+    @task_list = TaskList.new(task_list_params)
+  end
+
+  private
+
+  def task_list_params
+    params.require(:task_list).permit(:user_id, :status)
+  end
 end
