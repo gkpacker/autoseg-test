8ae8f8adeaebad907c3f77267ecee2efd43a60cc
Gabriel Packer <ggkpacker@gmail.com>
2018-05-07 15:18:45 -0300

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_07_172401) do
+ActiveRecord::Schema.define(version: 2018_05_07_180538) do
 
   create_table "subtasks", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_05_07_172401) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
+    t.integer "state", default: 0
     t.index ["user_id"], name: "index_task_lists_on_user_id"
   end
 
