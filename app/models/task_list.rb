912fa20d162b5943a59994701e989dabf001818c
Gabriel Packer <ggkpacker@gmail.com>
2018-05-07 16:05:43 -0300

    validates tasks for nested attributes

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -5,5 +5,5 @@ class TaskList < ApplicationRecord
 
   has_many :tasks
 
-  accepts_nested_attributes_for :tasks
+  accepts_nested_attributes_for :tasks, reject_if: :all_blank, allow_destroy: true
 end
