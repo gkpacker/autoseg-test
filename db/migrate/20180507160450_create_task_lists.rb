9c3474ac48dd419a65fa3294166d3335a4919f97
Gabriel Packer <ggkpacker@gmail.com>
2018-05-07 13:05:05 -0300

    generate task list model

---
+++ db/migrate/20180507160450_create_task_lists.rb
@@ -0,0 +1,9 @@
+class CreateTaskLists < ActiveRecord::Migration[5.2]
+  def change
+    create_table :task_lists do |t|
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
