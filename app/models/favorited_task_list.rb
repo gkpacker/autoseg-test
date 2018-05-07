23299f14918c34c4b826d77deca04eb37fd2f133
Gabriel Packer <ggkpacker@gmail.com>
2018-05-07 15:29:21 -0300

    generate favorited task lists with references

--- app/models/favorited_task_list.rb
+++ app/models/favorited_task_list.rb
@@ -1,2 +1,4 @@
 class FavoritedTaskList < ApplicationRecord
+  belongs_to :user
+  belongs_to :task_list
 end
--- db/migrate/20180507182856_create_favorited_task_lists.rb
+++ db/migrate/20180507182856_create_favorited_task_lists.rb
@@ -1,6 +1,8 @@
 class CreateFavoritedTaskLists < ActiveRecord::Migration[5.2]
   def change
     create_table :favorited_task_lists do |t|
+      t.references :user, foreign_key: true
+      t.references :task_list, foreign_key: true
 
       t.timestamps
     end
