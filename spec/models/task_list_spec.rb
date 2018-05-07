966bfbf97bcfe8db49ca2564b4bd028f204f527e
Gabriel Packer <ggkpacker@gmail.com>
2018-05-07 13:15:06 -0300

    test task list model

--- spec/models/task_list_spec.rb
+++ spec/models/task_list_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe TaskList, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "its not valid whitout a user assigned" do
+    expect(TaskList.create).to_not be_valid
+  end
+
+  it "its valid whit a user assigned" do
+    user = User.create(email: "test@test.com", password: "123456")
+
+    expect(TaskList.create(user_id: user.id)).to be_valid
+  end
 end
