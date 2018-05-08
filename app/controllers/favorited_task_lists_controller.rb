ad2de547b11410deff28a6c2c1ca00c73ec2bd22
Gabriel Packer <ggkpacker@gmail.com>
2018-05-08 01:45:17 -0300

    add final card style to favorited task lists

--- app/controllers/favorited_task_lists_controller.rb
+++ app/controllers/favorited_task_lists_controller.rb
@@ -1,5 +1,18 @@
 class FavoritedTaskListsController < ApplicationController
   def index
-    @favorited_task_lists = FavoritedTaskLists.where(user_id: current_user.id)
+    @favorited_task_lists = FavoritedTaskList.where(user_id: current_user.id)
+  end
+
+  def create
+    favorited = FavoritedTaskList.new(favorited_task_lists_params)
+    favorited.user = current_user
+    favorited.title = favorited.task_list.title
+    redirect_to favorited if favorited.save
+  end
+
+  private
+
+  def favorited_task_lists_params
+    params.require(:favorited_task_list).permit(:task_list_id)
   end
 end
--- db/migrate/20180508032423_add_title_to_favorited_task_lists.rb
+++
@@ -1,5 +0,0 @@
-class AddTitleToFavoritedTaskLists < ActiveRecord::Migration[5.2]
-  def change
-    add_column :favorited_task_lists, :title, :string
-  end
-end
