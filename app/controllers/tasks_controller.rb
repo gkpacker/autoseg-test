dd93b00d2c464d8e4c4816e2f849c06fe314da0c
Gabriel Packer <ggkpacker@gmail.com>
2018-05-09 13:00:40 -0300

    route to mark as done and subtasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:done, :destroy]
   def new
     @task = Task.new
   end
@@ -7,9 +8,25 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
   end
 
+  def done
+    @task.done!
+    redirect_to @task.task_list
+  end
+
+  def destroy
+    task_list = @task.task_list
+    @task.destroy
+    redirect_to task_list
+  end
+
   private
 
+  def set_task
+    id = params[:id] || params[:task_id]
+    @task = Task.find(id)
+  end
+
   def task_params
-    params.require(:task).permit(:title, :task_list_id)
+    params.require(:task).permit(:title, :task_list_id, :subtasks)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   root to: 'task_lists#index'
 
   devise_for :users
-  resource :users, only: :show
 
+  resource :users, only: :show
   resources :task_lists do
     get 'favorited_task_lists', to: 'favorited_task_lists#update_favorited_task_list'
   end
   resources :favorited_task_lists, only: [:index]
-  resources :tasks, only: [:edit, :update, :destroy]
+  resources :tasks, only: [:edit, :update, :destroy] do
+    get 'done', to: 'tasks#done'
+    resources :subtasks, only: [:new, :create, :edit, :update, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
