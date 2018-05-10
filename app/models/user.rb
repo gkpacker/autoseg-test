42eec5c72b65c47d9f20b2832445a62bed10827f
Gabriel Packer <ggkpacker@gmail.com>
2018-05-10 18:16:17 -0300

    Add dependent destroy to users tasklists

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :task_lists
-  has_many :favorited_task_lists
+  has_many :task_lists, dependent: :destroy
+  has_many :favorited_task_lists, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
