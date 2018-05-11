4a00ab918070dca2db79c9ad0a490386d4423221
Gabriel Packer <ggkpacker@gmail.com>
2018-05-11 03:32:30 -0300

    Notify user when a favorited task list is updated

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -7,4 +7,27 @@ class TaskList < ApplicationRecord
   accepts_nested_attributes_for :tasks, allow_destroy: true
 
   validates :title, presence: true
+
+  after_update :broadcast_notification
+
+
+  def broadcast_notification
+    ActionCable.server.broadcast("notifications", {
+      notification_partial: ApplicationController.renderer.render(
+        partial: "task_lists/notifications",
+        locals: { task_list: self }
+      ),
+      task_list: self,
+      users_who_favorited: users_who_favorited
+    })
+  end
+
+  def users_who_favorited
+    users = []
+    FavoritedTaskList.where(task_list: self).each do |favorited_task_list|
+      users << favorited_task_list.user.id
+    end
+    users
+  end
+
 end
